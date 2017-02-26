fb0bede899f97d886b264e57388b24a0e94c24ba
Branimir Ricko <rickobranimir@gmail.com>
2017-02-26 14:17:34 +0100

    Models MVVM structure

--- BucketApp/BucketApp/Models/Item.cs
+++ BucketApp/BucketApp/Models/Item.cs
@@ -1,19 +1,32 @@
 ﻿namespace BucketApp.Models
 {
     public class Item : BaseDataObject
-	{
-		string text = string.Empty;
-		public string Text
-		{
-			get { return text; }
-			set { SetProperty(ref text, value); }
-		}
+    {
+        string text = string.Empty;
+        public string Text
+        {
+            get { return text; }
+            set { SetProperty(ref text, value); }
+        }
 
-		string description = string.Empty;
-		public string Description
-		{
-			get { return description; }
-			set { SetProperty(ref description, value); }
-		}
-	}
+        string description = string.Empty;
+        public string Description
+        {
+            get { return description; }
+            set { SetProperty(ref description, value); }
+        }
+        string location = string.Empty;
+        public string Location
+        {
+            get { return location; }
+            set { SetProperty(ref location, value); }
+        }
+        string ownerName = string.Empty;
+        public string OwnerName
+        {
+            get { return ownerName; }
+            set { SetProperty(ref ownerName, value); }
+        }
+
+    }
 }
