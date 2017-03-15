d0109fc034b5f8d3ea01ad36de5db68682cd8525
Josip Paulik <josip.paulik@outlook.com>
2017-03-15 18:19:04 +0100

    Create CoreModels folder and core classes. Rename solution.

---
+++ OQPYManager/Models/CoreModels/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace OQPYManager.Models.CoreModels
+{
+    public static class Constants
+    {
+        public static int MinimumRating = 0;
+        public static int MaximumRating = 10;
+    }
+}
---
+++ OQPYManager/Models/CoreModels/Review.cs
@@ -0,0 +1,28 @@
+﻿namespace OQPYManager.Models.CoreModels
+{
+    public class Review
+    {
+        
+        public string Comment { get; set; }
+
+        public string Id { get; set; }
+
+        private int _rating;
+
+        public int Rating
+        {
+            get { return _rating; }
+            set
+            {
+                if (value < Constants.MinimumRating)
+                    _rating = Constants.MinimumRating;
+                else if (value > Constants.MaximumRating)
+                    _rating = Constants.MaximumRating;
+                else
+                    _rating = value;
+            }
+        }
+
+
+    }
+}
