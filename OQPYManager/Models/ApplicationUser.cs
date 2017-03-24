c8b8a57f94ad197a0d003695f9cb96b9b1fc1316
Branimir Ričko <rickobranimir@gmail.com>
2017-03-24 04:37:11 +0100

    Revert "Finish core features"

--- OQPYManager/Models/ApplicationUser.cs
+++ OQPYManager/Models/ApplicationUser.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using OQPYManager.Models.CoreModels;
 
 namespace OQPYManager.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public Venue Venue { get; set; }
     }
 }
