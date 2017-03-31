2ebbb005ac06815e9cb81fed9323634d059e0e45
Branimir Ricko <rickobranimir@gmail.com>
2017-03-31 14:28:06 +0200

    Helper log class

---
+++ OQPYBot/Helper/Log.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OQPYBot1.Helper
+{
+    public static class Log
+    {
+        public static void BasicLog(string about, object logData, SeverityLevel Sl)
+        {
+            if (Sl == SeverityLevel.Information)
+            {
+#if DEBUG
+                return;
+#endif
+            }
+            var telemetry = new Microsoft.ApplicationInsights.TelemetryClient();
+            telemetry.TrackTrace("Log",
+                           SeverityLevel.Information,
+                           new Dictionary<string, string> { { about, logData.ToString() } });
+            System.Diagnostics.Debug.WriteLine($"{about}({Sl}): {logData}");
+        }
+    }
+}
