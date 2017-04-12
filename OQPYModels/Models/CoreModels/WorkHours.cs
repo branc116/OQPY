71f8651b82384505242e2a1fc445d72ad380c2e8
Branimir Ricko <rickobranimir@gmail.com>
2017-04-12 02:51:49 +0200

    Fixes for Recursive loop

--- OQPYModels/Models/CoreModels/WorkHours.cs
+++ OQPYModels/Models/CoreModels/WorkHours.cs
@@ -18,23 +18,23 @@ namespace OQPYModels.Models.CoreModels
         public List<WorkTime> WorkTimes { get; set; }
 
         public bool IsWorking { get; set; }
-        public WorkTime Mondy => WorkTimes?[0] ?? null;
-        public WorkTime Tuesday => WorkTimes?[1] ?? null;
-        public WorkTime Wednesday => WorkTimes?[2] ?? null;
-        public WorkTime Thursday => WorkTimes?[3] ?? null;
-        public WorkTime Friday => WorkTimes?[4] ?? null;
-        public WorkTime Saturday => WorkTimes?[5] ?? null;
-        public WorkTime Sunday => WorkTimes?[6] ?? null;
+        public WorkTime Mondy => WorkTimes != null &&  WorkTimes.Count >= 1 ? WorkTimes[0] : null;
+        public WorkTime Tuesday => WorkTimes != null && WorkTimes.Count >= 2 ? WorkTimes[1] : null;
+        public WorkTime Wednesday => WorkTimes != null && WorkTimes.Count >= 3 ? WorkTimes[2] : null;
+        public WorkTime Thursday => WorkTimes != null && WorkTimes.Count >= 4 ? WorkTimes[3] : null;
+        public WorkTime Friday => WorkTimes != null && WorkTimes.Count >= 5 ? WorkTimes[4] : null;
+        public WorkTime Saturday => WorkTimes != null && WorkTimes.Count >= 6 ? WorkTimes[5] : null;
+        public WorkTime Sunday => WorkTimes != null && WorkTimes.Count >= 7 ? WorkTimes[6] : null;
 
         public Dictionary<DayOfWeek, WorkTime> WholeWeek => new Dictionary<DayOfWeek, WorkTime>()
         {
-            {DayOfWeek.Monday, WorkTimes?[0] ?? null },
-            {DayOfWeek.Tuesday, WorkTimes?[1] ?? null },
-            {DayOfWeek.Wednesday, WorkTimes?[2] ?? null },
-            {DayOfWeek.Thursday, WorkTimes?[3] ?? null },
-            {DayOfWeek.Friday, WorkTimes?[4] ?? null },
-            {DayOfWeek.Saturday, WorkTimes?[5] ?? null },
-            {DayOfWeek.Sunday, WorkTimes?[6] ?? null },
+            {DayOfWeek.Monday, Mondy },
+            {DayOfWeek.Tuesday, Tuesday },
+            {DayOfWeek.Wednesday, Wednesday },
+            {DayOfWeek.Thursday, Thursday },
+            {DayOfWeek.Friday, Friday },
+            {DayOfWeek.Saturday, Saturday },
+            {DayOfWeek.Sunday, Sunday },
         };
 
         public WorkHours()
@@ -57,6 +57,18 @@ namespace OQPYModels.Models.CoreModels
             this.VenueId = venue.Id;
         }
 
+        public void FixLoops()
+        {
+            if (this.WorkTimes != null)
+                foreach (var _ in WorkTimes)
+                    _.WorkHours = this;
+        }
+        public void UnFixLoops()
+        {
+            if (this.WorkTimes != null)
+                foreach (var _ in WorkTimes)
+                    _.WorkHours = null;
+        }
         public static IEnumerable<WorkHours> RandomWorkHours(int n, Venue venue)
         {
             return from _ in new string(' ', n)
@@ -64,5 +76,6 @@ namespace OQPYModels.Models.CoreModels
                    let hours = workHours.WorkTimes = WorkTime.RandomWorkTime(7, workHours).ToList()
                    select workHours;
         }
+
     }
 }
