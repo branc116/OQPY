980f296112109db36371cf08ac08400f7f81b73d
Branimir Ricko <rickobranimir@gmail.com>
2017-05-02 01:52:12 +0200

    Facebook location added

---
+++ OQPYModels/Models/CoreModels/FacebookLocation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OQPYModels.Models.CoreModels
+{
+
+    public class FacebookLocation
+    {
+        public Geo geo { get; set; }
+        public override string ToString() => geo.ToString();
+    }
+
+    public class Geo
+    {
+        public float elevation { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public string type { get; set; }
+        public override string ToString() => $"({latitude}, {longitude})";
+    }
+
+
+}
