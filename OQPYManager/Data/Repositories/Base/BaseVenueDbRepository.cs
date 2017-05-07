a96bba76128ba06d167ac05af321ce9b578682d8
Branimir Ricko <rickobranimir@gmail.com>
2017-05-07 15:05:51 +0200

    More base classes for one for ResourceDbRepository and One to only have db context

--- OQPYManager/Data/Repositories/Base/BaseVenueDbRepository.cs
+++ OQPYManager/Data/Repositories/Base/BaseVenueDbRepository.cs
@@ -11,27 +11,5 @@ namespace OQPYManager.Data.Repositories.Base
         public BaseVenueDbRepository(ApplicationDbContext context) : base(context)
         {
         }
-
-        public abstract Task AddVenueAsync(Venue venue);
-
-        public abstract Task AddVenuesAsync(params Venue[] venues);
-
-        public abstract Task AddVenuesAsync(IEnumerable<Venue> venues);
-
-        public abstract Task<Venue> FindVenueAsync(string key);
-
-        public abstract IEnumerable<Venue> GetAllVenues();
-
-        public abstract IEnumerable<Venue> GetAllVenues(params string[] includedParams);
-
-        public abstract IEnumerable<Venue> GetAllVenues(string includedParams);
-
-        public abstract IEnumerable<Venue> GetVenues(params Func<Venue, bool>[] filters);
-
-        public abstract IEnumerable<Venue> GetVenues(string includedParams, params Func<Venue, bool>[] filters);
-
-        public abstract Task RemoveAsync(string key);
-
-        public abstract Task UpdateAsync(Venue venue);
     }
 }
