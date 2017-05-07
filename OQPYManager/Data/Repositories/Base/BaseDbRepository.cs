77ccecd36bbdeea1c9f810bec8d1713119626431
Branimir Ricko <rickobranimir@gmail.com>
2017-05-07 14:58:51 +0200

    Implemented generic mehodes in repository

--- OQPYManager/Data/Repositories/Base/BaseDbRepository.cs
+++ OQPYManager/Data/Repositories/Base/BaseDbRepository.cs
@@ -2,25 +2,96 @@
 using OQPYModels.Models.CoreModels;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using static OQPYManager.Helper.Log;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace OQPYManager.Data.Repositories.Base
 {
-    public abstract class BaseDbRepository<T>: IBaseDbRepository<T> where T : ICoreModel<T>
+    public abstract class BaseDbRepository<T>: BaseContext, IBaseDbRepository<T> where T : class, ICoreModel<T>
     {
-        protected readonly ApplicationDbContext _context;
-
-        public BaseDbRepository(ApplicationDbContext context)
+        
+        private const string TAG = "BaseDb";
+        public BaseDbRepository(ApplicationDbContext context) :base(context)
         {
-            _context = context;
-
-            if ( !_context.Venues.Any() )
-            {
-                OnCreate();
-            }
+            
         }
 
-        public abstract void OnCreate();
+        public abstract Task OnCreate();
 
         public abstract Task<IQueryable<T>> Filter(T like);
+        public virtual async Task AddAsync(T venue)
+        {
+            await _context.AddAsync(venue);
+        }
+        public virtual async Task AddAsync(params T[] venues)
+        {
+            await _context.AddRangeAsync(venues);
+        }
+        public virtual async Task AddAsync(IEnumerable<T> venues)
+        {
+            await _context.AddRangeAsync(venues);
+        }
+        /// <summary>
+        /// can't make it generic...
+        /// </summary>
+        /// <returns></returns>
+        public abstract IEnumerable<T> GetAll();
+        /// <summary>
+        /// can't make it generic...
+        /// </summary>
+        /// <param name="includedParams"></param>
+        /// <returns></returns>
+        public abstract IEnumerable<T> GetAll(params string[] includedParams);
+        /// <summary>
+        /// can't make it generic...
+        /// </summary>
+        /// <param name="includedParams"></param>
+        /// <returns></returns>
+        public abstract IEnumerable<T> GetAll(string includedParams);
+        /// <summary>
+        /// can't make it generic...
+        /// </summary>
+        /// <param name="includedParams"></param>
+        /// <returns></returns>
+        public abstract IEnumerable<T> Get(params Func<T, bool>[] filters);
+        /// <summary>
+        /// can't make it generic...
+        /// </summary>
+        /// <param name="includedParams"></param>
+        /// <returns></returns>
+        public abstract IEnumerable<T> Get(string includedParams, params Func<T, bool>[] filters);
+        public virtual async Task<T> FindAsync(string key)
+        {
+            T item = await _context.FindAsync<T>(key);
+            if (item == null )
+            {
+                BasicLog(TAG, $"Id not found = {key}", SeverityLevel.Error);
+                throw new KeyNotFoundException(key);
+            }
+            return item;
+        }
+        public virtual async Task RemoveAsync(string key)
+        {
+            T item = await FindAsync(key);
+            _context.Remove(item);
+        }
+        public virtual async Task UpdateAsync(T venue)
+        {
+            _context.Update(venue);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch ( Exception ex )
+            {
+                BasicLog(TAG, $"Id not found", SeverityLevel.Error);
+                throw new KeyNotFoundException();
+            }
+
+
+        }
     }
 }
