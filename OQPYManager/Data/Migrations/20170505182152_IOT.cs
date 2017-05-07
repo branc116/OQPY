05237e5e5d48bd4200d5bf1f9ae8b626dcc2caeb
Branimir Ricko <rickobranimir@gmail.com>
2017-05-07 15:04:17 +0200

    new migration for IOT enabled resources

---
+++ OQPYManager/Data/Migrations/20170505182152_IOT.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OQPYManager.Data.Migrations
+{
+    public partial class IOT : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IOTEnabled",
+                table: "Resources",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OQPYed",
+                table: "Resources",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecreteCode",
+                table: "Resources",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IOTEnabled",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "OQPYed",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "SecreteCode",
+                table: "Resources");
+        }
+    }
+}
