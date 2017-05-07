c19927a34afc3cea599a3ddd40355313ab25a07e
Branimir Ricko <rickobranimir@gmail.com>
2017-05-07 15:06:11 +0200

    More interfaces

--- OQPYManager/Data/Repositories/Interfaces/IBaseContext.cs
+++ OQPYManager/Data/Repositories/Interfaces/IBaseContext.cs
@@ -1,11 +1,11 @@
-﻿using OQPYModels.Models.CoreModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace OQPYManager.Data.Repositories.Interfaces
 {
-    public interface IVenuesDbRepository: IBaseDbRepository<Venue>
+    public interface IBaseContext
     {
     }
 }
--- OQPYManager/Data/Repositories/Interfaces/IBaseDbRepository.cs
+++ OQPYManager/Data/Repositories/Interfaces/IBaseDbRepository.cs
@@ -1,12 +1,69 @@
 ﻿using OQPYModels.Models.CoreModels;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OQPYManager.Data.Repositories.Interfaces
 {
-    public interface IBaseDbRepository<T> where T : ICoreModel<T>
+    public interface IBaseDbRepository<T>: IBaseContext where T : ICoreModel<T>
     {
-        void OnCreate();
+        Task OnCreate();
+        Task AddAsync(T item);
+
+        Task AddAsync(params T[] items);
+
+        Task AddAsync(IEnumerable<T> items);
+
+        IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// Get all venues with included parameters
+        /// </summary>
+        /// <param name="includedParams">list of included parameters e.g. "Resources"</param>
+        /// <returns>list of venues with included parameters</returns>
+        IEnumerable<T> GetAll(params string[] includedParams);
+
+        /// <summary>
+        /// Get all venues with included parameters
+        /// </summary>
+        /// <param name="includedParams">list of included parameters seperated with ';' e.g. "Resources;Resources.Reservations"</param>
+        /// <returns>list of venues with included parameters</returns>
+        IEnumerable<T> GetAll(string includedParams);
+
+        /// <summary>
+        /// Gets venues based on passed filters.
+        /// </summary>
+        /// <param name="filters">Filters a venue must pass.</param>
+        /// <returns>Venues that passed all filters.</returns>
+        IEnumerable<T> Get(params Func<T, bool>[] filters);
+
+        /// <summary>
+        /// Gets venues based on passed filters.
+        /// </summary>
+        /// <param name="includedParams"></param>
+        /// <param name="filters">Filters a venue must pass.</param>
+        /// <returns>Venues that passed all filters.</returns>
+        IEnumerable<T> Get(string includedParams, params Func<T, bool>[] filters);
+
+        /// <summary>
+        /// Finds venue based on key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>Venue with the key or null.</returns>
+        Task<T> FindAsync(string key);
+
+        /// <summary>
+        /// Removes venue from database which has passed key.
+        /// </summary>
+        /// <param name="key">Passed key.</param>
+        Task RemoveAsync(string key);
+
+        /// <summary>
+        /// Update a venue in database with new values.
+        /// </summary>
+        /// <param name="venue">Venue which holds new values.</param>
+        Task UpdateAsync(T venue);
 
         Task<IQueryable<T>> Filter(T like);
     }
