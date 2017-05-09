1994f159df35d6c46c7617699aaad4bfb1af752b
Branimir Ricko <rickobranimir@gmail.com>
2017-05-09 11:51:10 +0200

    Some buttons work now

--- OQPYBot/Controllers/Helper/Constants.cs
+++ OQPYBot/Controllers/Helper/Constants.cs
@@ -1,9 +1,13 @@
 ﻿#define forceProd
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
 using Microsoft.Rest.Serialization;
 using Newtonsoft.Json;
 using OQPYClient.APIv03;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace OQPYBot.Controllers.Helper
 {
@@ -14,35 +18,103 @@ namespace OQPYBot.Controllers.Helper
                 "http://localhost:5000/";
 #else
                 "https://oqpymanager.azurewebsites.net/";
-#endif
-        public const string _propNames = "propertyNames";
-        public const string _name = "name";
-        public const string _email = "email";
-        public const string _location = "location";
-        public const string _facebooklocation = "Facebook Location";
-        public const string _channelId = "chId";
-        public const string _subsublocation = "SubSubLocation";
-        public const string _currentActiveVenues = "currentActiveVenues";
-        public const string _insideDialogKey = "insideProperty";
-        public const string _actionInfo = "Info";
-        public const string _actionComments = "Comments";
-        public const string _actionReservations = "Reservations";
 
-        public static readonly List<string> _propertyKeys = new List<string> { _name, _email, _location, _facebooklocation, _currentActiveVenues };
-        public static readonly List<string> _cardActions = new List<string> { _actionInfo, _actionComments, _actionReservations};
-        public static readonly List<string> _cardActionsYesNo = new List<string> { "yes", "no" };
+#endif
+        internal const string _propNames = "propertyNames";
+        internal const string _name = "name";
+        internal const string _email = "email";
+        internal const string _location = "location";
+        internal const string _timesUsed = "timesUsed";
+        internal const string _facebooklocation = "Facebook Location";
+        internal const string _subsublocation = "SubSubLocation";
+
+        internal const string _currentActiveVenues = "currentActiveVenues";
+
+        internal const string _insideDialogKey = "insideProperty";
+
+        internal const string _channelId = "chId";
+        internal const string _layoutCarousel = "carousel";
+
+        internal const string _actionInfo = "Info";
+        internal const string _actionComments = "Comments";
+        internal const string _actionReservations = "Reservations";
+        internal const string _actionResources = "Resources";
+        internal const string _actionAdd = "Add New";
+        internal const string _actionLike = "Like";
+        internal const string _actionDislike = "Dislike";
+        internal const string _actionReservateAfter = "Reservate after";
+        internal const string _actionReservateBefore = "reservate before";
+        internal const string _actionReadFull = "Read";
+        internal const string _actionNeatby = "Nearby";
+        internal const string _actionHelp = "Help";
+        internal const string _actionShareLocation = "Share location";
+        internal const string _actionSignIn = "Sign in";
+
+        internal const string _baseObj = "Base";
+        internal const string _venueObj = "Venue";
+        internal const string _resourcesObj = "Resources";
+        internal const string _commentsObj = "Comments";
+        internal const string _reservationsObj = "Reservation";
+        internal const string _locationObj = "Location";
+
+        internal const string _commandBaseHelp = _actionHelp + _baseObj;
+        internal const string _commandBaseNearby = _actionNeatby + _baseObj;
+        internal const string _commandBaseShareLocation = _actionShareLocation + _baseObj;
+        internal const string _commandSignIn = _actionSignIn + _baseObj;
+
+        internal const string _commandVenueInfo = _actionInfo + _venueObj;
+        internal const string _commandVenueComments = _actionComments + _venueObj;
+        internal const string _commandVenueReservations = _actionReservations + _venueObj;
+        internal const string _commandVenueResources = _actionResources + _venueObj;
+        internal const string _commandVenueLike = _actionLike + _venueObj;
+        internal const string _commandVenueDislike = _actionDislike + _venueObj;
+
+        internal const string _commandResourcesInfo = _actionInfo + _resourcesObj;
+        internal const string _commandResourcesRead = _actionReadFull + _resourcesObj;
+
+        internal const string _commandCommentsAdd = _actionAdd + _commentsObj;
+        internal const string _commandCommentsLike = _actionLike + _commentsObj;
+        internal const string _commandCommentsDislike = _actionDislike + _commentsObj;
+        internal const string _commandCommentsRead = _actionReadFull + _commentsObj;
+
+        internal const string _commandReservateAfter = _actionReservateAfter + _reservationsObj;
+        internal const string _commandReservateBefore = _actionReservateBefore + _reservationsObj;
+
+        internal static readonly List<string> _propertyKeys = new List<string> { _name, _email, _location, _facebooklocation, _currentActiveVenues };
+
+        internal static readonly List<string> _likeDislikeActions = new List<string> { _actionLike, _actionDislike };
+        internal static readonly List<string> _venueCardActions = new List<string> { _actionInfo, _actionComments, _actionReservations };
+        internal static readonly List<string> _resourcesCardActions = new List<string> { _actionInfo };
+        internal static readonly List<string> _commentsCardActions = new List<string> { _actionAdd };
+        internal static readonly List<string> _reservationsCardActions = new List<string> { _commandReservateBefore, _commandReservateAfter };
+        internal static readonly List<string> _hello = new List<string> { "Hello", "What can I do for you", "Oh hello", "What's up", "Hi" };
+
+        internal static readonly Dictionary<string, Func<IDialogContext, string, Task<IEnumerable<Attachment>>>> _processCommands = new Dictionary<string, Func<IDialogContext, string, Task<IEnumerable<Attachment>>>>()
+        {
+            { _commandVenueInfo, ProcessCommands.VenueInfo },
+            { _commandVenueComments, ProcessCommands.VenueComments  },
+            { _commandVenueReservations, ProcessCommands.VenueReservations },
+            { _commandVenueResources, ProcessCommands.VenueResources },
+            { _commandBaseHelp, ProcessCommands.BaseHelp },
+            { _commandBaseNearby, ProcessCommands.BaseSearchNearby }
+            //{ _commandResourcesInfo, ProcessCommands.ResourceInfo },
+        };
+
+        internal static readonly List<string> _cardActionsYesNo = new List<string> { "yes", "no" };
+
         public static readonly JsonSerializerSettings _safeDeserializationSettings = new JsonSerializerSettings
-            {
-                DateFormatHandling = DateFormatHandling.IsoDateFormat,
-                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
-                NullValueHandling = NullValueHandling.Ignore,
-                ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
-                ContractResolver = new ReadOnlyJsonContractResolver(),
-                Converters = new List<JsonConverter>
+        {
+            DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+            NullValueHandling = NullValueHandling.Ignore,
+            ReferenceLoopHandling = ReferenceLoopHandling.Serialize,
+            ContractResolver = new ReadOnlyJsonContractResolver(),
+            Converters = new List<JsonConverter>
                     {
                         new Iso8601TimeSpanConverter()
                     }
-            };
-        public static readonly MyAPI _api = new MyAPI(new Uri(_managerUri));
+        };
+
+        internal static readonly MyAPI _api = new MyAPI(new Uri(_managerUri));
     }
 }
---
+++ OQPYBot/Controllers/Helper/ProcessCommands.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using OQPYClient.APIv03;
+using OQPYModels.Models.CoreModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static OQPYBot.Controllers.Helper.Constants;
+using static OQPYBot.Controllers.Helper.Helper;
+
+namespace OQPYBot.Controllers.Helper
+{
+    public class ProcessCommands
+    {
+        public async static Task<IEnumerable<Attachment>> VenueInfo(IDialogContext context, string venueId)
+        {
+            var venue = await _api.ApiVenuesSingleGetAsync(venueId);
+            var totalResources = venue.Resources == null ? 0 : venue.Resources.Count;
+            var freeResources = venue.Resources == null ? 0 : venue.Resources.Where(i => i.OQPYed == false).Count();
+            var gotGeo = context.UserData.TryGetValue(_facebooklocation, out Geo geo);
+            var distanceString = gotGeo && venue.Location != null ? $"{venue.Location.ToKilometers(new Location(geo.longitude, geo.latitude))} km" : "Can't get distane";
+            var cards = new List<Attachment> {
+                new ThumbnailCard(_locationObj, distanceString, venue?.Location?.Adress ?? venue?.Location?.ToString() ?? "Can't get location").ToAttachment(),
+                new ThumbnailCard(_resourcesObj, null, $"{freeResources} free out of {totalResources}", null, MakeCardActions(venueId, _venueObj, _actionResources ).ToList()).ToAttachment(),
+                new ThumbnailCard(_reservationsObj, $"There are {venue?.Reservations.Count() ?? 0} reservations", null, null, MakeCardActions(venueId, _venueObj, _actionReservations).ToList()).ToAttachment(),
+                new ThumbnailCard(_commentsObj, $"There are {venue?.Reviews.Count() ?? 0} comments", null, null, MakeCardActions(venueId, _venueObj, _actionComments).ToList()).ToAttachment(),
+            };
+            return cards;
+        }
+
+        internal async static Task<IEnumerable<Attachment>> VenueReservations(IDialogContext context, string venueId)
+        {
+            var venue = await _api.ApiVenuesSingleGetAsync(venueId);
+            List<Attachment> cards = new List<Attachment>() {
+            new ThumbnailCard(_reservationsObj,
+                venue.Reservations == null ? $"There aren't any reservations in {venue.Name}" : $"There are {venue.Reviews.Count} Comments",
+                $"You can add a reservaion by clicking {_actionAdd} button",
+                null,
+                MakeCardActions(venueId, _reservationsObj, _actionAdd).ToList()).ToAttachment()
+            };
+            if ( venue.Reservations != null )
+            {
+                cards.AddRange(from _ in venue.Reservations.Take(9)
+                               select
+                               new ThumbnailCard(
+                                   _reservationsObj,
+                                   _.Resource.StuffName,
+                                   $"From ${_.StartReservationTime} to ${_.EndReservationTime}",
+                                   null,
+                                   MakeCardActions(
+                                      _.Id,
+                                      _reservationsObj,
+                                      _reservationsCardActions)
+                                      .ToList())
+                                .ToAttachment());
+            }
+            return cards;
+        }
+
+        internal async static Task<IEnumerable<Attachment>> VenueResources(IDialogContext context, string venueId)
+        {
+            var venue = await _api.ApiVenuesSingleGetAsync(venueId);
+            venue.FixLoops();
+            var totalResources = venue.Resources == null ? 0 : venue.Resources.Count;
+            var freeResources = venue.Resources == null ? 0 : venue.Resources.Where(i => i.OQPYed == false).Count();
+            var cards = new List<Attachment>
+            {
+                new ThumbnailCard(_resourcesObj, null, $"{freeResources} free out of {totalResources}").ToAttachment()
+            };
+            if ( venue.Resources != null )
+            {
+                cards.AddRange(
+                    from _ in venue.Resources.Take(9)
+                    select new ThumbnailCard(_.StuffName, _.OQPYed ? "Taken :/" : "Free :)", _.GetInfo(), null, MakeCardActions(_.Id, _resourcesObj, _actionReservations).ToList()).ToAttachment()
+                    );
+            }
+            return cards;
+        }
+
+        internal async static Task<IEnumerable<Attachment>> VenueComments(IDialogContext context, string venueId)
+        {
+            var venue = await _api.ApiVenuesSingleGetAsync(venueId);
+            List<Attachment> cards = new List<Attachment>() {
+            new ThumbnailCard(_commentsObj,
+                venue.Reviews == null ? $"There aren't any comments for {venue.Name}" : $"There are {venue.Reviews.Count} Comments",
+                $"You can add a review by clicking {_actionAdd} button",
+                null,
+                MakeCardActions(venueId, _commentsObj, _actionAdd).ToList()).ToAttachment()
+            };
+            if ( venue.Reviews != null )
+            {
+                cards.AddRange(from _ in venue.Reviews.Take(9)
+                               select
+                               new ThumbnailCard(
+                                   _commentsObj,
+                                   $"{_.Rating}/10",
+                                   _.Comment,
+                                   null,
+                                   MakeCardActions(
+                                      _.Id,
+                                      _commentsObj,
+                                      _likeDislikeActions
+                                      )
+                                      .ToList())
+                                .ToAttachment());
+            }
+            return cards;
+        }
+
+        internal async static Task<IEnumerable<Attachment>> BaseHelp(IDialogContext context, string arg2)
+        {
+            var attachemts = new List<Attachment>
+            {
+                new HeroCard("OQPY", "OQPY is a bot that helps you with your venue chices").ToAttachment(),
+                new HeroCard("Search venues", "Search venues by just typing the name of the venue you wanna search", null, null, MakeCardActions("arg2", _baseObj, _actionNeatby).ToList()).ToAttachment(),
+                new HeroCard("Share you location", "Make your searches more precise and select where you wanna search", null, null, MakeCardActions("arg2", _baseObj, _actionShareLocation).ToList()).ToAttachment(),
+                new HeroCard("Sign in", "Sign in so that you can make reservations", null, null, MakeCardActions("arg2", _baseObj, _actionSignIn).ToList()).ToAttachment(),
+            };
+            return attachemts;
+        }
+
+        internal async static Task<IEnumerable<Attachment>> BaseSearchNearby(IDialogContext context, string arg2)
+        {
+            await ProcessVenues(context, new SearchVenues());
+            return null;
+        }
+    }
+}
